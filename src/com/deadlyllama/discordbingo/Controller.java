52ad94532b3bd932622c6dbcd5ddaf034bed5766
DLMousey <tom@deadlyllama.com>
2020-01-19 21:19:17 +0000

    Add vicory-by-row win condition

--- src/com/deadlyllama/discordbingo/Controller.java
+++ src/com/deadlyllama/discordbingo/Controller.java
@@ -11,6 +11,9 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Controller {
 
@@ -28,6 +31,8 @@ public class Controller {
     protected StringValuesService stringValuesService = new StringValuesService();
     protected GameButtonsService gameButtonsService = new GameButtonsService(this.stringValuesService);
 
+    private Map<Integer, List<Integer>> selectedMap = new HashMap<>();
+
     public void initialize() {
         this.initialiseGameBoard();
     }
@@ -43,8 +48,15 @@ public class Controller {
     }
 
     private void initialiseGameBoard() {
+        selectedMap.clear();
+
         this.gameButtonsService.initialiseLabels();
         this.gameButtonsService.initialiseGameButtons();
+
+        selectedMap.put(0, new ArrayList<>());
+        selectedMap.put(1, new ArrayList<>());
+        selectedMap.put(2, new ArrayList<>());
+        selectedMap.put(3, new ArrayList<>());
 
         ArrayList<ToggleButton> buttons = gameButtonsService.getGameButtons();
 
@@ -84,16 +96,49 @@ public class Controller {
 
     private void onButtonClick(ActionEvent event) {
         ToggleButton button = (ToggleButton) event.getSource();
-        if (button.isSelected()) {
-            this.gameButtonsService.incrementSelectedItems();
-        } else {
-            this.gameButtonsService.decrementSelectedItems();
-        }
-
         HBox parentHbox = (HBox) button.getParent();
 
+        Integer row;
+        Integer column = parentHbox.getChildren().indexOf(button);
+
+        switch(parentHbox.getId()) {
+            case "rowFirst_hbox":
+                row = 0;
+                break;
+            case "rowSecond_hbox":
+                row = 1;
+                break;
+            case "rowThird_hbox":
+                row = 2;
+                break;
+            case "rowFourth_hbox":
+                row = 3;
+                break;
+            default:
+                row = null;
+                break;
+        }
+
+        if (row == null) {
+            throw new RuntimeException("Unable to determine row index");
+        }
+
+        List<Integer> values = selectedMap.get(row);
+
+        if (button.isSelected()) {
+            this.gameButtonsService.incrementSelectedItems();
+            values.add(column);
+        } else {
+            this.gameButtonsService.decrementSelectedItems();
+            values.remove(column);
+        }
+
         System.out.println(button.getText() + " was clicked, selected? : " + button.isSelected());
-        System.out.println("Button parent: " + parentHbox.getId() + " pos: " + parentHbox.getChildren().indexOf(button));
-        System.out.println("Selected items: " + this.gameButtonsService.getSelectedItems());
+        for (int i = 0; i < 4; i++) {
+            List<Integer> rowValues = selectedMap.get(i);
+            if (rowValues.size() == 4) {
+                System.out.println("BINGO!");
+            }
+        }
     }
 }
